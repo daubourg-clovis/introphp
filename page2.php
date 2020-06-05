1c6880a7bc431ca2e92f72edbc3941a33933d86a
daubourg-clovis <daubourg.clovis@gmail.com>
2020-06-05 10:14:38 +0200

    Update - correction erreur chargement de page
    mise en place d'un if isset pour tout le php pour ne pas avoir d'erreur au chargement car le formulaire n'as pas été validé une première fois

--- page2.php
+++ page2.php
@@ -6,31 +6,37 @@
     <title>Page 2</title>
 </head>
 <body>
-    <form action="page2.php" method="post" name="tables">
-    <select name="multiplication" id="multiplication">
-        <option value="" selected>Choisir une table de multiplication</option>
-        <option  value="Table du 1" >Table du 1</option>
-        <option  value="Table du 2">Table du 2</option>
-        <option  value="Table du 3">Table du 3</option>
-        <option  value="Table du 4">Table du 4</option>
-        <option  value="Table du 5">Table du 5</option>
-        <option  value="Table du 6">Table du 6</option>
-        <option  value="Table du 7">Table du 7</option>
-        <option  value="Table du 8">Table du 8</option>
-        <option  value="Table du 9">Table du 9</option>
-        <option  value="Table du 10">Table du 10</option>
+    <form action="page2.php" method="post">
+    <select name="multiplication" id="multiplication" method="post">
+        <option  value="choix" selected>Choisir une table de multiplication</option>
+        <option  value="1" >Table du 1</option>
+        <option  value="2">Table du 2</option>
+        <option  value="3">Table du 3</option>
+        <option  value="4">Table du 4</option>
+        <option  value="5">Table du 5</option>
+        <option  value="6">Table du 6</option>
+        <option  value="7">Table du 7</option>
+        <option  value="8">Table du 8</option>
+        <option  value="9">Table du 9</option>
+        <option  value="10">Table du 10</option>
     </select>
     <input type="submit" name="validate" value="OK">
     </form>
     <?php
-        $tables=array($_POST['tables']);
-        $number=x;
-        if(isset($_POST['validate'])){
-            echo 'Ceci est la table du '.$number.'';
-            foreach($_POST as $tables){
-                for ($i=1;$i<11;$i++)
-                $result=$i*$number;
-                echo '- '.$i.' x '.$number.' = '.$result.' <br>';
+        if(isset($_POST['multiplication'])){
+            $tables=$_POST['multiplication']; 
+            $number=intval($tables);
+            if(isset($_POST['validate'])){
+                if($tables!='choix'){
+                    echo 'Ceci est la table du '.$tables.' <br>';            
+                }
+                for ($i=0;$i<11;$i++){
+                    $result=$i*$number;
+                    if($tables!='choix'){
+                        echo '- '.$number.' x '.$i.' = '.$result.' <br>';
+                    }
+                }
+                
             }
         }
         
